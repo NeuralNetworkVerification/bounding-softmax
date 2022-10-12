6538907e8ffbb968c63966f87fb0258eb3473a46
anwu1219 <haozewu@stanford.edu>
2022-10-12 13:38:44 -0700

    weights of new networks

--- transformer_experiment/runExperiment.sh
+++ transformer_experiment/runExperiment.sh
@@ -5,19 +5,23 @@ SCRIPT=/home/haozewu/Projects/softmax/Marabou/resources/runMarabou.py
 
 NETWORK=./self-attention-small-sim.onnx
 
-for i in {0..5}
-#for i in {0..499}
+for t in linear lse1 lse2 er
 do
-    for e in 0.008 0.01 0.012 0.014 0.016
+    for e in 0.008 0.012 0.016 0.02
     do
-        for t in linear lse1 lse2 er
+        #for i in {0..2}
+        for i in {0..499}
         do
             sFile="$i"_"$e"_"$t".summary
-            $SCRIPT $NETWORK --dataset mnist -i $i -e $e --softmax-bound $t --summary-file="$sFile"
-            if grep -q unsat "$sFile"; then
+            $SCRIPT $NETWORK --dataset mnist -i $i -e $e --softmax-bound $t --summary-file="$sFile" --verbosity=0 --num-workers=8
+            if [ ! -f $sFile ]; then
+                echo $i $e $t misclassify >> results.csv
+            elif grep -q unsat "$sFile"; then
                 echo $i $e $t unsat >> results.csv
+                rm $sFile
             else
                 echo $i $e $t unknown >> results.csv
+                rm $sFile
             fi
         done
     done
